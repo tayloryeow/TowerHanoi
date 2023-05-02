daea552f23d501a37dd3452b9cbc9cf4af55141d
Taylor Stinson <tayloryeow@gmail.com>
2023-05-02 18:08:58 -0400

    added an untested scaffolding

--- _scafolding/README.md
+++ _scafolding/README.md
@@ -1,3 +1,1 @@
-# Whats this folder for? 
 
-Essentially stay out but this is where I have all the Taylor Code to generate the problem.
--- _scafolding/towers_of_hanoi.py
+++ _scafolding/towers_of_hanoi.py
@@ -7,10 +7,37 @@ Assumptions for the moment
 '''
 
 class Disc:
-    pass
+    #Creates a disc of a specified size
+    def __init__(self, size):
+        self.size = size
 
 class Pillar:
-    pass
+    #Creates a pillar in a certain peg location.
+    def __init__(self, peg_position, list_of_disc_sizes):
+        self.peg_position = peg_position
 
+        #Fill the pillar with discs of the appropriate size in the appropriate order.
+
+
+'''
+Game class. 
+Holds the entire game logics, moves, state, and view.
+Only touchable classes will be the movement, checking victory, and setup stuff.
+'''
 class TowersOfHanoi:
-    pass
+    # Initialize a random towers of hanoi problem.
+    # - Checks the number of discs given is valid for the num pillars.
+    def __init__(self, num_pillars = 3, num_discs = 3):
+        # Store a reference to all the game state classes. Just a list of pillars really.
+        self.game_state = []
+
+        #Create the given of pillars
+        for curr_pillar_number in range (0, num_pillars):
+            #Manipulate the parameters for the Pillar construction
+            disc_list = [1, 2, 3]
+            newly_created_pillar = Pillar(curr_pillar_number, disc_list)
+            self.game_state.append(newly_created_pillar)
+
+            #generate a list of discs
+            #Randomize said discs
+            #Feed into pillar
