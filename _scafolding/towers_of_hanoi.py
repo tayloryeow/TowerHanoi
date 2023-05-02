d314fe18849d8396f68698ddfff4b45963191aa4
Taylor Stinson <tayloryeow@gmail.com>
2023-05-02 17:46:18 -0400

    Added basic class declarations

---
+++ _scafolding/towers_of_hanoi.py
@@ -0,0 +1,16 @@
+'''
+Class file for keeping state, visuals, and control of the problem.
+
+Assumptions for the moment
+> Only works for 3 pillars
+> Up to 6 discs
+'''
+
+class Disc:
+    pass
+
+class Pillar:
+    pass
+
+class TowersOfHanoi:
+    pass
